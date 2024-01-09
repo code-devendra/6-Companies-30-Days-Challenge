56e5ca93a02264379c16fc819ff9dca547c5b7d6
Devendra Khinchi <107763868+code-devendra@users.noreply.github.com>
2024-01-09 19:28:26 +0530

    WiggleSort-Microsoft.java

--- WiggleSort-Microsoft.java
+++ WiggleSort-Microsoft.java
@@ -5,7 +5,9 @@ class Solution {
         for(int i = 0; i < n; i++) {
             temp[i] = nums[i];
         }
+        
         Arrays.sort(temp);
+        
         int i = 1;
         int j = n-1;
         while(i < n) {
