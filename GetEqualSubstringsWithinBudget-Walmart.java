f8d0f2b64460834ff629c691f53601612e086fe7
Devendra Khinchi <107763868+code-devendra@users.noreply.github.com>
2024-01-14 21:29:07 +0530

    Create GetEqualSubstringsWithinBudget-Walmart.java

---
+++ GetEqualSubstringsWithinBudget-Walmart.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+        int i = 0, j;
+        for(j = 0; j < n; j++) {
+            maxCost -= Math.abs(s.charAt(j) - t.charAt(j));
+            if(maxCost < 0) {
+                maxCost += Math.abs(s.charAt(i) - t.charAt(i));
+                ++i;
+            }
+        }
+
+        return j-i;
+    }
+}
